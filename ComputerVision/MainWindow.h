83622166c4c792fcf4822a17a84daf55e93c1a11
itzLilix <112267705+itzLilix@users.noreply.github.com>
2026-09-06 17:02:16 +0400

    initial commit

---
+++ ComputerVision/MainWindow.h
@@ -0,0 +1,5 @@
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+};
---
+++ ComputerVision/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <QtWidgets/QApplication>
+#include "MainWindow.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	QApplication app(argc, argv);
+
+    MainWindow window;
+    window.show();
+
+    return app.exec();
+}
